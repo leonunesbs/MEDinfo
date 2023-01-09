00ed3115dfc5a76dcc1c375c6742cd1f5b8a4f1e
Leonardo Nunes <leonunesbs@gmail.com>
2023-01-09 12:26:23 -0300

    feat(add): gtag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from 'next/app';
 import { ChakraBaseProvider } from '@chakra-ui/react';
 import { Fonts } from '@/components/atoms';
+import Script from 'next/script';
 import { SessionProvider } from 'next-auth/react';
 import theme from '@/styles/theme';
 import { trpc } from '@/utils/trpc';
@@ -12,6 +13,19 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
         <Component {...pageProps} />
         <Fonts />
       </ChakraBaseProvider>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GA_MEASUREMENT_ID}`}
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){window.dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', ${process.env.GA_MEASUREMENT_ID});
+          `}
+      </Script>
     </SessionProvider>
   );
 }
